00eba6838cdb8d0d06dff0e8bb5ebffeb7641075
salwahamidah <salwahamidah00@gmail.com>
2021-08-18 03:27:02 +0300

    added an gitignore file

---
+++ index.js
@@ -0,0 +1,31 @@
+const { SSL_OP_CIPHER_SERVER_PREFERENCE } = require('constants');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+//delivery  order data
+const deliveryorderArray = [{
+    itemname:'clothes',
+    sendersname:'Abubaker Yusuf',
+    senderscontact:'0897464536',
+    senderslocation:'Kampala',
+    receiversname:'Umar Lut',
+    receiverscontact:'0836654749',
+    receiverslocation:'Adjumani'
+
+}]
+
+
+app.post('/deliveryorder',(req,res)=>{
+    const newdeliveryorder = req.body;
+    deliveryorderArray.push(newdeliveryorder);
+
+    res.send('Parcel Delivery Order Successfully Created !!!!!!')
+})
+
+
+app.listen(6000,()=>{
+    console.log('server is listening on port 6000 ....')
+})
