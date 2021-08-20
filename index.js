08771bdb386d9108c165f8a50ca55618b3db0dbb
Roland-Sankara <roland1sankara@gmail.com>
2021-08-20 20:25:33 +0300

    fix(delete node_modules folder)

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
-app.use(express.json())
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+
+// database connection
+const dbURI = 'mongodb+srv://Maratah:test1234@cluster0.q5jue.mongodb.net/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => console.log('Database Connection Established . . .'))
+  .catch((err) => console.log('Database Connection Failed . . .'));
+
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+
+
+/* === Delivery order Routes === **/
 
 //delivery  order data
 const deliveryorderArray = [{
@@ -17,16 +44,13 @@ const deliveryorderArray = [{
 
 }]
 
-
 app.get('/deliveryorder',(req,res)=>{
     res.send(deliveryorderArray)
 })
 
-
 app.get('/deliveryorder/:id',(req,res)=>{
     const deliveryorder = deliveryorderArray.find((deliveryorder)=>{return deliveryorder.id === parseInt(req.params.id)})
 })
-
 
 app.post('/deliveryorder',(req,res)=>{
     const newdeliveryorder = req.body.newdeliveryorder;
@@ -34,7 +58,6 @@ app.post('/deliveryorder',(req,res)=>{
 
     res.send('Parcel Delivery Order Successfully Created !!!!!!')
 })
-
 
 app.delete('/deliveryorder/:id', (req, res)=>{
     const newdeliveryorder = deliveryorderArray.find((newdeliveryorder)=>{return newdeliveryorder.id === parseInt(req.params.id)});
@@ -47,7 +70,6 @@ app.delete('/deliveryorder/:id', (req, res)=>{
     }
 })
 
-
 app.patch('/deliveryorder/:id',(req,res)=>{
     const newdeliveryorder = deliveryorderArray.find((newdeliveryorder)=>{
         return newdeliveryorder.id===parseInt(req.params.id)
@@ -58,6 +80,9 @@ app.patch('/deliveryorder/:id',(req,res)=>{
 })
 
 
-app.listen(6000,()=>{
-    console.log('server is listening on port 6000 ....')
+
+const PORT = process.env.PORT || 3000;
+// Server
+app.listen(PORT,()=>{
+    console.log(`Server is listening on port ${PORT} ....`);
 })
