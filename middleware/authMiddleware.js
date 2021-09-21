96defb6141cc56df1c06348733be720ea144ba67
Roland-Sankara <roland1sankara@gmail.com>
2021-09-22 00:33:17 +0300

    fix(pick token from headers not cookies)

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,29 @@
 const jwt = require('jsonwebtoken');
 
 const requireAuth = (req, res, next) => {
-  const token = req.cookies.jwt;
-  
-  // check json web token exists & is verified
-  if (token) {
-    jwt.verify(token, 'courier-backend-fullstack-cohort1', (err, decodedToken) => {
-      if (err) {
-        console.log(err.message);
-        res.status(400).json({
-          "error": error.message
-        });
-      } else {
-        console.log(decodedToken.id);
-        req.userID = decodedToken.id;
-        next();
-      }
-    });
-  } else {
-    res.status(404).json({error: "Authorization Token missing. Please Login.."});
+  // const token = req.cookies.jwt;
+  const header = req.headers.authorization;
+
+  if(header){
+    if(header.startsWith('Bearer')){
+      const token = header.split(' ')[1];
+      // check json web token exists & is verified
+      jwt.verify(token, 'courier-backend-fullstack-cohort1', (err, decodedToken) => {
+        if (err) {
+          res.status(400).json({
+            error: "Token is Invalid ... "
+          });
+        } else {
+          console.log(decodedToken.id);
+          req.userID = decodedToken.id;
+          next();
+        }
+      });
+    }
+  }else{
+    res.json({Error: "Missing Authorisation header"})
   }
+   
 };
 
 
